4c5cc42fc1725ce4e97d0d7666b19411ca608af4
SzabiSch <szabiweb21@gmail.com>
2021-11-01 15:21:53 +0100

    Some Practice with VUE

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,51 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    Hi
+    {{ name_first }}
+
+    <ul>
+      <li
+        v-for="(todo, index) in todos"
+        :key="todo.id"
+        :class="{ finishedTask: todo.finished }"
+      >
+        <input type="checkbox" :checked="todo.finished" />
+        <a :href="'?id=' + todo.id">zeigen</a> {{ ++index }}.{{ todo.name }}
+      </li>
+    </ul>
+    <button v-on:click="test">TEST</button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data: () => ({
+    name_first: "Szabi",
+    todos: [
+      { id: 1, finished: false, name: "VUE JS leren" },
+      { id: 2, finished: true, name: "Code Beispiele üben" },
+      { id: 3, finished: false, name: "Git commit" },
+      { id: 4, finished: true, name: "ein weiteres Todo" },
+    ],
+  }),
+  methods: {
+    test() {
+      alert("Hallooo!!!");
+    },
+  },
+};
 </script>
 
 <style>
+.finishedTask {
+  text-decoration: line-through;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  text-align: left;
   color: #2c3e50;
   margin-top: 60px;
 }
